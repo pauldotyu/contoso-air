399dcb42784abedd45da0cb620aa4927496317e2
Paul Yu <paul.d.yu@gmail.com>
2024-10-07 21:51:58 -0700

    initial port

--- README.md
+++ README.md
@@ -1,57 +1,22 @@
-# Project Name
+# contoso-air
 
-(short, 1-3 sentenced, description of the project)
+## Prerequisites
 
-## Features
-
-This project framework provides the following features:
-
-* Feature 1
-* Feature 2
-* ...
+- Node.js 22.0.0 or later
 
 ## Getting Started
 
-### Prerequisites
+First, clone the repository then run the following commands:
 
-(ideally very short, if any)
+```bash
+# change directory
+cd src/frontend
 
-- OS
-- Library version
-- ...
+# install dependencies
+npm install
 
-### Installation
+# run the app
+npm start
+```
 
-(ideally very short)
-
-- npm install [package name]
-- mvn install
-- ...
-
-### Quickstart
-(Add steps to get up and running quickly)
-
-1. git clone [repository clone url]
-2. cd [repository name]
-3. ...
-
-
-## Demo
-
-A demo app is included to show how to use the project.
-
-To run the demo, follow these steps:
-
-(Add steps to start up the demo)
-
-1.
-2.
-3.
-
-## Resources
-
-(Any additional resources or related projects)
-
-- Link to supporting information
-- Link to similar sample
-- ...
+Browse to `http://localhost:3000` to see the app.
---
+++ src/frontend/app.js
@@ -0,0 +1,77 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var path = require("path");
+var logger = require("morgan");
+var Handlebars = require("handlebars");
+var exphbs = require("express-handlebars");
+var session = require("express-session");
+var flash = require("express-flash");
+var favicon = require("serve-favicon");
+var passport = require("./config/passport.config");
+var i18n = require("i18n");
+
+var app = express();
+
+// Configure i18n
+i18n.configure({
+  objectNotation: true,
+  locales: ["en", "es"],
+  directory: path.join(__dirname, "locales"),
+});
+
+// Register i18n as a middleware
+app.use(i18n.init);
+
+// Register the i18n helper with Handlebars
+var hbs = exphbs.create({
+  extname: ".hbs",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+  partialsDir: path.join(__dirname, "views/partials"),
+  helpers: {
+    i18n: function (str) {
+      return new Handlebars.SafeString(i18n.__(str));
+    },
+  },
+});
+
+// view engine setup
+app.engine("hbs", hbs.engine);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(favicon(__dirname + "/public/assets/favicon.ico"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cookieParser());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(flash());
+app.use(session({ secret: "secret", ressave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes"));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
